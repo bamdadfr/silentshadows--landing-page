a5a33b0f0070d47fd6427767e0f816d1accc5d69
Bamdad Sabbagh <hi@bamdad.fr>
2022-09-30 17:06:00 +0200

    fix: Add scaling above tablets

--- src/components/title.component.tsx
+++ src/components/title.component.tsx
@@ -3,14 +3,19 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import logo from '../../public/images/title.png';
 import {FadeAnimation} from '../animations/fade.animation';
+import {mediaQueries} from '../app/styles/breakpoints';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  transform: translateY(1rem);
   z-index: 20;
+  transform: translateY(1rem);
+
+  ${mediaQueries.above.tablet} {
+    transform: translateY(1rem) scale(1.3);
+  }
 `;
 
 export function TitleComponent(): ReactElement {

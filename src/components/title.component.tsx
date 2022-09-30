4cc969fb002a33ead728c7b72d54bde31bb17da0
Bamdad Sabbagh <hi@bamdad.fr>
2022-09-30 17:04:37 +0200

    fix: Remove title scaling + Add global padding

--- src/components/title.component.tsx
+++ src/components/title.component.tsx
@@ -3,23 +3,14 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import logo from '../../public/images/title.png';
 import {FadeAnimation} from '../animations/fade.animation';
-import {mediaQueries} from '../app/styles/breakpoints';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  transform: translateY(1rem) scale(1.3);
+  transform: translateY(1rem);
   z-index: 20;
-`;
-
-const Spacer = styled.div`
-  width: 20%;
-
-  ${mediaQueries.above.mobile} {
-    width: 0;
-  }
 `;
 
 export function TitleComponent(): ReactElement {
@@ -27,13 +18,11 @@ export function TitleComponent(): ReactElement {
     <>
       <FadeAnimation delay={0}>
         <Container>
-          <Spacer />
           <Image
             src={logo}
             alt=""
             // layout="responsive"
           />
-          <Spacer />
         </Container>
       </FadeAnimation>
     </>
